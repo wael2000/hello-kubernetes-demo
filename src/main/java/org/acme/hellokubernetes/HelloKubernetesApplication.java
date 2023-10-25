06ea3ea5a224fb5f53cb3aeda33efe823a0eaf8a
wael2000 <weldoami@redhat.com>
2023-10-25 14:06:19 +0200

    first commit

---
+++ src/main/java/org/acme/hellokubernetes/HelloKubernetesApplication.java
@@ -0,0 +1,13 @@
+package org.acme.hellokubernetes;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HelloKubernetesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HelloKubernetesApplication.class, args);
+	}
+
+}
